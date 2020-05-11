f6fa3ff49db4badf23c92feda12fa8b4f491b9ad
BuildTools <unconfigured@null.spigotmc.org>
2020-05-11 13:57:40 -0400

    Fixed chicken/egg problem.

--- Server/src/main/java/com/clinvest/migraine/server/AdminServlet.java
+++ Server/src/main/java/com/clinvest/migraine/server/AdminServlet.java
@@ -16,48 +16,36 @@ import com.clinvest.migraine.server.data.Database;
 @WebServlet("/admin/*")
 public class AdminServlet extends HttpServlet
 {
-  private static final long serialVersionUID = 1L;
-  private static final Logger LOG = LogManager.getLogger("MIGRAINE");
-  
+  private static final long   serialVersionUID = 1L;
+  private static final Logger LOG              = LogManager.getLogger("MIGRAINE");
+
   @Override
-  public void init() {
+  public void init()
+  {
     LOG.debug("Admin servlet initialized.");
   }
-  
+
   /*
-   * /admin/initdb?auth=<token>
+   * /admin/initdb
    */
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
   {
-    String pathInfo = request.getPathInfo();
-    String[] parts = pathInfo.split("/");
-    String authToken = request.getParameter("auth");
-    // Check auth token
-    if (null != authToken && Authorize.checkAuthToken(authToken))
+    LOG.debug("Initializing migraine database.");
+    try
     {
-      LOG.debug("Initializing migraine database.");
-      try
-      {
-        response.setContentType("text/plain");
-        response.setStatus(HttpServletResponse.SC_OK);
-        Database.initialize();
-        response.getWriter().println("Database initialized.");
-        LOG.debug("Database initialized.");
-      }
-      catch (Exception e)
-      {
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        LOG.error("Error initializing migraine database.", e);
-      }  
+      response.setContentType("text/plain");
+      response.setStatus(HttpServletResponse.SC_OK);
+      Database.initialize();
+      response.getWriter().println("Database initialized.");
+      LOG.debug("Database initialized.");
     }
-    else
+    catch (Exception e)
     {
-      LOG.info("Unauthorized request.");
-      response.sendError(401, "Unauthorized");
+      response.setContentType("text/html");
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      LOG.error("Error initializing migraine database.", e);
     }
   }
-    
-    
+
 }
