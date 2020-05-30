1ad5ce83d02cee0af86f8c67e090600baf37fd94
Phil Grim <phil@insufficient-light.com>
2020-05-30 19:37:09 -0400

    Beginning to add scripts for login and registration functionality.

---
+++ Migraine-App/Assets/Scripts/Model/RequestResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace clinvest.migraine.Model
+{
+    [Serializable]
+    public class LoginRequest
+    {
+        public string username;
+        public string password;
+    }
+
+    [Serializable]
+    public class LoginResponse
+    {
+        public string userId;
+        public string authToken;
+    }
+
+    [Serializable]
+    public class RegistrationRequest
+    {
+        public string firstName;
+        public string lastName;
+        public long birthDate;
+        public string email;
+        public string password;
+    }
+
+    [Serializable]
+    public class RegistrationResponse
+    {
+        public bool success;
+        public bool message;
+    }
+
+}
---
+++ Migraine-App/Assets/Scripts/View/YearDropdownView.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace clinvest.migraine.View {
+
+public class YearDropdownView : MonoBehaviour
+{
+    public TMP_Dropdown YearDropdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       List<string> opts = new List<string>();
+       int year = 2020;
+       while (year > 1910)
+       {
+           year = year - 1;
+           opts.Add(year.ToString());    
+       }
+       YearDropdown.AddOptions(opts);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+}
