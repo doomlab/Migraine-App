0dedc210f01c3c371be9571e9c43db56cb64107a
phil <phil@DESKTOP-G2PVJV0>
2020-06-24 12:39:50 -0400

    Remove reference to Simple JSON.

--- Server/src/main/java/com/clinvest/migraine/server/DiaryServlet.java
+++ Server/src/main/java/com/clinvest/migraine/server/DiaryServlet.java
@@ -1,15 +1,10 @@
 package com.clinvest.migraine.server;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONObject;
 
 @WebServlet("/diary")
 public class DiaryServlet extends HttpServlet
