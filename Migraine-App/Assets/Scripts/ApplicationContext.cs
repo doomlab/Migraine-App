7719ce0195dc084a1d218be1fd6d5607ce3e83f4
phil <phil@DESKTOP-G2PVJV0>
2020-06-24 12:39:33 -0400

    Global storage for user ID, user name, and auth token.

---
+++ Migraine-App/Assets/Scripts/ApplicationContext.cs
@@ -0,0 +1,42 @@
+﻿public static class ApplicationContext
+{
+    private static string userName;
+    private static string userId;
+    private static string authToken;
+
+    public static string UserName 
+    {
+        get 
+        {
+            return userName;
+        }
+        set 
+        {
+            userName = value;
+        }
+    }
+
+    public static string UserId 
+    {
+        get 
+        {
+            return userId;
+        }
+        set 
+        {
+            userId = value;
+        }
+    }
+
+    public static string AuthToken 
+    {
+        get 
+        {
+            return authToken;
+        }
+        set 
+        {
+            authToken = value;
+        }
+    }
+}
