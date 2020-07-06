f2d592612cb313a2c6b779a210ba38d322b87a49
Phil Grim <phil@insufficient-light.com>
2020-07-06 00:29:55 -0400

    Renamed YearDropdownView to be more specific.  Added DiagnosisYearDropdownView.  Not updating scene because of the errors in my editor.

---
+++ Migraine-App/Assets/Scripts/View/BirthYearDropdownView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace clinvest.migraine.View
+{
+
+    public class BirthYearDropdownView : MonoBehaviour
+    {
+        private const int MAX_YEAR = 2005;
+        private const int MIN_YEAR = 1910;
+
+        public TMP_Dropdown BirthYearDropdown;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            List<string> opts = new List<string>();
+            int year = MAX_YEAR;
+            while (year > MIN_YEAR)
+            {
+                year = year - 1;
+                opts.Add(year.ToString());
+            }
+            BirthYearDropdown.AddOptions(opts);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
