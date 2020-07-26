267d54fad8147b7325d298e1d0cc97c8f2bb3b6b
Phil Grim <phil@insufficient-light.com>
2020-07-26 17:46:50 -0400

    Create some default studies.

--- Server/src/main/java/com/clinvest/migraine/server/data/Database.java
+++ Server/src/main/java/com/clinvest/migraine/server/data/Database.java
@@ -11,11 +11,15 @@ import org.apache.commons.codec.digest.UnixCrypt;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 
 public class Database
 {
   private static final Logger LOG = LogManager.getLogger("MIGRAINE");
   private static Database instance;
+  private Gson gson;
 
   /** Default root user for the NUA application. */
   private static final String[][] USERS = { { "ef8ed6b2-9d4f-43c3-b253-ef4d2eb544f5", "Admin", "User",
@@ -39,7 +43,13 @@ public class Database
       {  "Aleve",   "naproxen sodium" }
   };
 
-
+  private static final String[] STUDIES = {
+    "{ \"name\": \"Development and Test Study\", \"description\": \"Study to use during application development and testing.\", \"sponsor\": \"Harrisburg University\", " + 
+    "  \"investigator\": \"Phil Grim\", \"contact\": \"pgrim@harrisburgu.edu\", \"startDate\": \"Jan 15, 2020 12:00:00 AM\",  \"endDate\": \"Dec 15, 2020 12:00:00 AM\" }", 
+    "{ \"name\": \"Migraine Scale Study\", \"description\": \"Study to test migraine scales.\", \"sponsor\": \"Clinvest\", " + 
+    "  \"investigator\": \"Dr. Erin Buchanan\", \"contact\": \"ebuchanan@harrisburgu.edu\", \"startDate\": \"Aug 15, 2020 12:00:00 AM\",  \"endDate\": \"Jul 15, 2021 12:00:00 AM\" }" 
+  };
+  
   public static void initialize()
   {
     if (null == instance)
@@ -50,17 +60,27 @@ public class Database
 
   private Database()
   {
+    gson = new GsonBuilder().setLenient().create();
     // Create the session factory, which will ensure tables get created.
     LOG.debug("Checking for database tables.");
     HibernateUtils.getSessionFactory();
     // Check to see if the root user exists. If not, create all of the standard users and roles.
     LOG.debug("Checking for standard data.");
-    User root = User.getById(UUID.fromString(USERS[0][0]));
+    User root = null;
+    try
+    {
+      root = User.getById(UUID.fromString(USERS[0][0]));
+    }
+    catch (Exception e)
+    {
+      LOG.debug("No database detected.");
+    }
     if (null == root)
     {
       createUsers();
       createRoles();
       createUserRoles();
+      createStudies();
     }
     updateMedicationList();
   }
@@ -77,10 +97,10 @@ public class Database
       u.setEmail(data[3]);
       u.setBirthDate(Timestamp.valueOf(LocalDateTime.now().minusDays(1)));
       u.setPassword(UnixCrypt.crypt(data[4], "MA"));
-      u.setCreated(Timestamp.valueOf(LocalDateTime.now()));
+
       if ("confirm".equals(data[5]))
       {
-        u.setConfirmed(u.getCreated());
+        u.setConfirmed(Timestamp.valueOf(LocalDateTime.now()));
       }
 
       User.save(u);
@@ -96,7 +116,6 @@ public class Database
       Role r = new Role();
       r.setName(data[0]);
       r.setDescription(data[1]);
-      r.setCreated(Timestamp.valueOf(LocalDateTime.now()));
       
       Role.save(r);
     }
@@ -111,7 +130,7 @@ public class Database
       UserRole ur = new UserRole();
       User u = User.getById(UUID.fromString(data[0]));
       ur.setUser(u);
-      ur.setCreated(Timestamp.valueOf(LocalDateTime.now()));
+
       Role r = Role.getByName(data[1]);
       if (null != r)
       {
@@ -125,6 +144,18 @@ public class Database
       }
     }
     LOG.debug("User roles created.");
+  }
+  
+  private void createStudies()
+  {
+    for (int i = 0; i < STUDIES.length; i++)
+    {
+      String data = STUDIES[i];
+      Study s = gson.fromJson(data, Study.class);
+      
+      Study.save(s);
+    }
+    LOG.debug("Studies created.");
   }
   
   private void updateMedicationList()
@@ -146,7 +177,7 @@ public class Database
         Medication m = new Medication();
         m.setName(data[0]);
         m.setFormulary(data[1]);
-        m.setCreated(Timestamp.valueOf(LocalDateTime.now()));
+
         Medication.save(m);
       }
     }
