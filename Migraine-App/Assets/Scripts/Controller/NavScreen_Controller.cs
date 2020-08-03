64e7184ba2726d5d3634385a60cf5e624ec4ec89
Wgrim42 <wgrim@my.Harrisburgu.edu>
2020-08-03 14:34:04 -0400

    Nav Screen Prototype added

---
+++ Migraine-App/Assets/Scripts/Controller/NavScreen_Controller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using clinvest.migraine.Model;
+using UnityEngine.SceneManagement;
+
+namespace clinvest.migraine.Controller
+{
+    public class NavScreen_Controller : MonoBehaviour
+    {
+        //Buttons
+        public Button Sandwich;
+
+        //Panels
+        public GameObject NavPanel;
+
+        public void SandwichButtonPressed()
+        {
+            if (NavPanel.active == false)
+            {
+                NavPanel.SetActive(true);
+            }
+            else
+            {
+                NavPanel.SetActive(false);
+            }
+
+        }
+    }
+    
+}
