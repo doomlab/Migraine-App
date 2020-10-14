a485cce152ddac5373d585d98f6158792585fe76
Phil Grim <pgrim@harrisburgu.edu>
2020-10-14 01:05:01 -0400

    Added controller for setting up proper active panels at startup.

---
+++ Migraine-App/Assets/Scripts/Controller/StartupController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace clinvest.migraine.Controller
+{
+
+    public class StartupController : MonoBehaviour
+    {
+        public GameObject LoginPanel;
+        public GameObject RegistrationPanel;
+        public GameObject PasswordResetPanel;
+        public GameObject HomePanel;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            PasswordResetPanel.SetActive(false);
+            RegistrationPanel.SetActive(false);
+            if (null != ApplicationContext.AuthToken)
+            {
+                HomePanel.SetActive(true);
+                ApplicationContext.ActivePanel = HomePanel;
+                LoginPanel.SetActive(false);
+            }
+            else
+            {
+                HomePanel.SetActive(false);
+                LoginPanel.SetActive(true);
+                ApplicationContext.ActivePanel = LoginPanel;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
