3e2acc408a43a9ae68ca10daf47304dbac54c15d
Phil Grim <pgrim@harrisburgu.edu>
2020-11-14 11:16:18 -0500

    Added a place to keep global constants.

---
+++ Migraine-App/Assets/Scripts/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace clinvest.migraine
+{
+
+    public class Constants
+    {
+        public int GRADIENT_1 = 0xfee5d9;
+        public int GRADIENT_2 = 0xfcae91;
+        public int GRADIENT_3 = 0xfb6a4a;
+        public int GRADIENT_4 = 0xde2d26;
+        public int GRADIENT_5 = 0xa50f15;
+    }
+}
