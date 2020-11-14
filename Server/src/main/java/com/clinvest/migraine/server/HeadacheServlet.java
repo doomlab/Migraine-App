d812e2faf5c5f15d9da9ba14f74e41aa4e0154f0
PGrim <PGrim@PGRIMG5>
2020-11-14 15:46:20 -0500

    Added endpoint for FAMS aggregate data.

--- Server/src/main/java/com/clinvest/migraine/server/HeadacheServlet.java
+++ Server/src/main/java/com/clinvest/migraine/server/HeadacheServlet.java
@@ -21,6 +21,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.clinvest.migraine.server.data.DiaryEntry;
+import com.clinvest.migraine.server.data.FamsEntry;
 import com.clinvest.migraine.server.data.User;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -55,7 +56,12 @@ public class HeadacheServlet extends HttpServlet {
         if (pathInfo.contains("get_days"))
         {
           getDays(request, response);
-        } else
+        } 
+        else if (pathInfo.contains("get_fams"))
+        {
+            getFams(request, response);
+        } 
+        else
         {
           LOG.debug("Invalid request.");
           response.sendError(400, "Invalid request.");
@@ -85,7 +91,7 @@ public class HeadacheServlet extends HttpServlet {
     String userId = null;
     if (null != data)
     {
-      HeadacheDaysRequest re = gson.fromJson(data, HeadacheDaysRequest.class);
+      HeadacheRequest re = gson.fromJson(data, HeadacheRequest.class);
       userId = re.getUserId();
     }
     if (null == userId)
@@ -200,6 +206,114 @@ public class HeadacheServlet extends HttpServlet {
 
   }
 
+  
+  protected void getFams(HttpServletRequest request, HttpServletResponse response) throws IOException
+  {
+    User           user   = null;
+
+    BufferedReader reader = request.getReader();
+    String         data   = IOUtils.toString(reader);
+    LOG.debug("getFams: " + data);
+
+    String userId = null;
+    if (null != data)
+    {
+      HeadacheRequest re = gson.fromJson(data, HeadacheRequest.class);
+      userId = re.getUserId();
+    }
+    if (null == userId)
+    {
+      LOG.debug("Invalid FAMS aggregate request: missing user id");
+      response.sendError(400, "Invalid request.");
+      return;
+    }
+    user = User.getById(UUID.fromString(userId));
+    if (null == user)
+    {
+      LOG.debug("Invalid FAMS aggregate request: User id not found.");
+      response.sendError(400, "Invalid request.");
+      return;
+    }
+    
+    FAMSAggregateResponse rep = new FAMSAggregateResponse();
+    rep.setUserId(userId);
+    
+    FamsEntry last = FamsEntry.getLastEntry(user);
+    if (null != last)
+    {
+        List<Integer> list = new ArrayList<Integer>();
+        // concentrate_questions <- c("Q13", "Q15")
+        int x1 = last.getQ13() == null? 0 : last.getQ13();
+        int x2 = last.getQ13() == null? 0 : last.getQ15();
+        int y = (int)Math.ceil(((double) (x1 + x2)) / 2);
+        list.add(y);
+    
+        // frequency_questions <- c("Q2", "Q5")
+        x1 = last.getQ2() == null? 0 : last.getQ2();
+        x2 = last.getQ5() == null? 0 : last.getQ5();
+        y = (int)Math.ceil(((double) (x1 + x2)) / 2);
+        list.add(y);
+        
+        // medicine_questions <- c("Q10", "Q17")
+        x1 = last.getQ10() == null? 0 : last.getQ10();
+        x2 = last.getQ17() == null? 0 : last.getQ17();
+        y = (int)Math.ceil(((double) (x1 + x2)) / 2);
+        list.add(y);
+        
+        // normal_questions <- c("Q4", "Q16")
+        x1 = last.getQ4() == null? 0 : last.getQ4();
+        x2 = last.getQ16() == null? 0 : last.getQ16();
+        y = (int)Math.ceil(((double) (x1 + x2)) / 2);
+        list.add(y);
+        
+        // pain_questions <- c("Q8", "Q12")
+        x1 = last.getQ8() == null? 0 : last.getQ8();
+        x2 = last.getQ12() == null? 0 : last.getQ12();
+        y = (int)Math.ceil(((double) (x1 + x2)) / 2);
+        list.add(y);
+        
+        // productivity_questions <- c("Q6", "Q14")
+        x1 = last.getQ6() == null? 0 : last.getQ6();
+        x2 = last.getQ14() == null? 0 : last.getQ14();
+        y = (int)Math.ceil(((double) (x1 + x2)) / 2);
+        list.add(y);
+
+        // self_questions <- c("Q7", "Q18")
+        x1 = last.getQ7() == null? 0 : last.getQ7();
+        x2 = last.getQ18() == null? 0 : last.getQ18();
+        y = (int)Math.ceil(((double) (x1 + x2)) / 2);
+        list.add(y);
+        
+        // social_questions <- c("Q9", "Q11")
+        x1 = last.getQ9() == null? 0 : last.getQ9();
+        x2 = last.getQ11() == null? 0 : last.getQ11();
+        y = (int)Math.ceil(((double) (x1 + x2)) / 2);
+        list.add(y);
+        
+        // associated_questions <- c("Q1", "Q3")
+        x1 = last.getQ1() == null? 0 : last.getQ1();
+        x2 = last.getQ3() == null? 0 : last.getQ3();
+        y = (int)Math.ceil(((double) (x1 + x2)) / 2);
+        list.add(y);
+    
+        rep.setAggregateStatus(list);
+    }
+    
+    
+    
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+
+    // create HTML response
+    PrintWriter  responder = response.getWriter();
+    StringWriter writer    = new StringWriter();
+
+    writer.append(gson.toJson(rep));
+    LOG.debug(writer.toString());
+    responder.append(writer.toString());
+
+  }
+  
   protected int todayIndex()
   {
     Calendar cal = new GregorianCalendar();
@@ -212,7 +326,7 @@ public class HeadacheServlet extends HttpServlet {
         cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR);
   }
 
-  static class HeadacheDaysRequest
+  static class HeadacheRequest
   {
     String userId;
 
@@ -240,6 +354,33 @@ public class HeadacheServlet extends HttpServlet {
       public void setHeadacheDays(List<Integer> headacheDays)
       {
         this.headacheDays = headacheDays;
+      }
+      
+      public String getUserId()
+      {
+        return userId;
+      }
+
+      public void setUserId(String userId)
+      {
+        this.userId = userId;
+      }
+      
+  }
+  
+  static class FAMSAggregateResponse
+  {
+      String userId;
+      List<Integer> aggregateStatus;
+
+      public List<Integer> getAggregateStatus()
+      {
+        return aggregateStatus;
+      }
+
+      public void setAggregateStatus(List<Integer> aggregateStatus)
+      {
+        this.aggregateStatus = aggregateStatus;
       }
       
       public String getUserId()
